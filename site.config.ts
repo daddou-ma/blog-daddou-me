3cbb1c03f59d846c725c144cfc7570f27429740f
DADDOU Mohamed El Amine <mea.daddou@gmail.com>
2022-07-04 21:12:06 +0100

    Set Config

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '6a1bf37c0ad44a8e8b944ca2acf81d35',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Daddou Mohamed El Amine - Software Engineer',
+  domain: 'daddou.me',
+  author: 'Daddou Mohamed El Amine',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Daddou Mohamed El Amine - Software Engineer',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'daddou_ma',
+  github: 'daddou-ma',
+  linkedin: 'daddou-me',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
@@ -53,7 +53,7 @@ export default siteConfig({
   // navigationStyle: 'custom',
   // navigationLinks: [
   //   {
-  //     title: 'About',
+  //     title: 'Aboutss',
   //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
   //   },
   //   {
